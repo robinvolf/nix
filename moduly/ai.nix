59161d27ed144c7ca713e41d50f7b439260b0e5b
Robin Volf <xvolfr00@stud.fit.vutbr.cz>
2026-03-12 09:27:00 +0100

    Přidán aichat a whisper

--- moduly/ai.nix
+++ moduly/ai.nix
@@ -2,14 +2,16 @@
   services.ollama = {
     enable = true;
 
-    # Akcelerace pro NVIDIA grafické karty
-    acceleration = "cuda";
-
     # Aby naslouchal i připojením, které jdou zvenku
-    openFirewall = true;
-    host = "0.0.0.0";
+    # openFirewall = true;
+    # host = "0.0.0.0";
 
     # Optional: preload models, see https://ollama.com/library
     # loadModels = [ "llama3.2:3b" "deepseek-r1:1.5b"];
   };
+
+  environment.systemPackages = with pkgs; [
+    aichat # Lepší rozhraní než ollama se spoustou dalších fičurek
+    whisper-cpp # Transkript audio -> text
+  ]
 }
