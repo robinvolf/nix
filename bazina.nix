48fe6e694cc2347099e35f6bc5620728bb38e8ed
Robin Volf <xvolfr00@stud.fit.vutbr.cz>
2026-04-19 16:22:43 +0200

    Konec hraní, na bažině to nefunguje

--- bazina.nix
+++ bazina.nix
@@ -8,7 +8,6 @@
       ./moduly/gui_programy.nix
       ./moduly/keyd.nix
       ./moduly/braille.nix
-      ./moduly/ai.nix
     ];
 
   # Nix konfigurace
@@ -124,9 +123,6 @@
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
-
-  nixpkgs.config.allowUnfree = true; # Kvůli CUDA akceleraci 😢
-  services.ollama.package = inputs.nixpkgsFresh.legacyPackages."x86_64-linux".ollama-cuda;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
--- moduly/ai.nix
+++ moduly/ai.nix
@@ -2,11 +2,11 @@
   services.ollama = {
     enable = true;
     environmentVariables = {
-      OLLAMA_CONTEXT_LENGTH = "64000"; # Pro agenty, potřebujou hodně velký kontext
+      OLLAMA_CONTEXT_LENGTH = "32000"; # Pro agenty, potřebujou hodně velký kontext
     };
 
     # Aby naslouchal i připojením, které jdou zvenku
-    openFirewall = true;
+    # openFirewall = true;
 
     # Optional: preload models, see https://ollama.com/library
     # loadModels = [ "llama3.2:3b" "deepseek-r1:1.5b"];
